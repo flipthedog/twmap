a3c380ea446db694bd2550ad54a6e1d4e0536138
Floris <florisvanrossum@gmail.com>
2025-02-06 21:50:40 -0500

    created some better maps

--- twmap/map/map.py
+++ twmap/map/map.py
@@ -10,7 +10,13 @@ class Map:
 
     def __init__(self):
         
-        self.background_color = "#58761b"
+        self.cell_color = "#58761b"
+        self.background_color = "#436213"
+        
+        self.dull_cell_color = "#0c2909"
+        self.dull_background_color = "#395436"
+        self.dull_colors = True
+
         self.tw_color = "#edd8ad"
 
         self.village_color = "#823c0a"
@@ -18,10 +24,9 @@ class Map:
 
         self.grid_color = "#000000"
 
-        self.center_x = 500
-        self.center_y = 500
-
         self.world_origin = 500
+        self.world_height = 1000
+        self.world_width = 1000
 
         self.show_grid = True
         self.show_center_lines = True
@@ -31,66 +36,76 @@ class Map:
         self.max_x = 0
         self.max_y = 0
 
-        self.image_height = 1000
-        self.image_width = 1000
-
         self.zoom = 3
 
-    def calculate_zoom(self, villages: List[VillageModel]):
+        self.cell_size = 4
+        self.spacing = 1
 
-        self.max_x = max(village.x_coord for village in villages)
-        self.max_y = max(village.y_coord for village in villages)
-
-        self.zoom = self.image_height / max(self.max_x, self.max_y)
-        print(f"Zoom: {self.zoom}")
-    
-    def draw_grid(self, image: Image, color: str):
-        
-        draw = ImageDraw.Draw(image)
-        
-        for i in range(self.world_origin, self.image_height, 100):
-
-            j = i - self.world_origin
-
-            print(f"Drawing line at {j}")
-
-            draw.line([0, j, self.image_height, j], fill=color, width=1)
-            draw.line([j, 0, j, self.image_height], fill=color, width=1)
-        
-    def draw_center_lines(self, image: Image, color: str):
-        
-        draw = ImageDraw.Draw(image)
-        
-        # draw center lines along center x and y
-        draw.line([self.image_height/2, 0, self.image_height/2, self.image_height], fill=color, width=2)
-        draw.line([0, self.image_height/2, self.image_width, self.image_height/2], fill=color,  width=2)
-
+        self.image_height = self.world_height * (self.cell_size + self.spacing)
+        self.image_width = self.world_width * (self.cell_size + self.spacing)
 
     def draw(self, villages: List[VillageModel]):
-        
 
-        # Change image size to 1000x1000
-        image = Image.new("RGB", (self.image_height, self.image_width), self.background_color)        
+        # draw a grid pattern with each box representing a village
+
         
-        # draw villages 
+        if self.dull_colors:
+            cell_color = self.dull_cell_color
+            background_color = self.dull_background_color
+        else:
+            cell_color = self.cell_color
+            background_color = self.background_color
+        
+        image = Image.new("RGB", (self.image_height, self.image_width), background_color)
+        
         draw = ImageDraw.Draw(image)
 
-        for village in villages:
-            x = (village.x_coord - self.center_x) * self.zoom + self.center_x
-            y = (village.y_coord - self.center_y) * self.zoom + self.center_y
+        for i in range(0, self.world_height):
 
-            color = self.village_color
+            for j in range(0, self.world_width):
+
+                # print(f"Drawing cell at {i}, {j}")
+
+                x = i * (self.cell_size + self.spacing)
+                y = j * (self.cell_size + self.spacing)
+
+                # print(f"Drawing cell at {x}, {y}")
+
+                draw.rectangle([x, y, x+self.cell_size - self.spacing, y+self.cell_size - self.spacing], fill=cell_color)
+        
+
+        for village in villages:
+
+            x = village.x_coord * (self.cell_size + self.spacing)
+            y = village.y_coord * (self.cell_size + self.spacing)
+
             if village.playerid == 0 and self.show_barbarians:
                 color = self.barbarian_color
-            village_size = self.zoom 
-            
-            draw.rectangle([x, y, x+village_size, y+village_size], fill=color)
+            else: 
+                color = self.village_color
+
+            draw.rectangle([x, y, x+self.cell_size - self.spacing, y+self.cell_size - self.spacing], fill=color)
 
         if self.show_grid:
-            self.draw_grid(image, self.grid_color)
+            self.draw_grid(image, self.grid_color, 100)
         
-        if self.show_center_lines:
-            self.draw_center_lines(image, self.grid_color)
+        # crop around the center
+        image = self.crop_image(image, 200)
 
         image.show()
 
+    def crop_image(self, image: Image, spacing: int):
+
+        return image.crop(((self.world_origin - spacing) * (self.cell_size + self.spacing), (self.world_origin - spacing) * (self.cell_size + self.spacing), (self.world_origin + spacing) * (self.cell_size + self.spacing), (self.world_origin + spacing) * (self.cell_size + self.spacing)))
+
+    def draw_grid(self, image: Image, color: str, grid_spacing: int):
+        
+        draw = ImageDraw.Draw(image)
+
+        for i in range(0, self.world_height, grid_spacing):
+            x = i * (self.cell_size + self.spacing) - 1
+            draw.line([x, 0, x, self.image_height], fill=color, width=1)
+        
+        for j in range(0, self.world_width, grid_spacing):
+            y = j * (self.cell_size + self.spacing) - 1
+            draw.line([0, y, self.image_width, y], fill=color, width=1)
