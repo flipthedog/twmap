689c4baa347eed6c38f6bc9a01fc8c374bc40ca2
Floris <florisvanrossum@gmail.com>
2025-02-06 23:48:18 -0500

    added samples

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-https://github.com/aschenkuttel/tribalwars-api/tree/master
+# TWMaps
+
+Creating pretty maps for Tribal Wars with Python. Uses the TW API to retrieve world village, player and tribe files. 
+
+
+# Results
+
+The following is a sample of images
+
+## World 144
+![World 144]('images/top_players.png')
+![World 144]('images/top_tribes.png')
--- twmap/api/data.py
+++ twmap/api/data.py
@@ -1,1 +1,41 @@
+import requests
+import os
 
+class TWAPI: 
+
+    def __init__(self, world: int, language: str):
+        
+        self.world = world
+        self.url = f"https://en{self.world}.tribalwars.net/game.php"
+
+        self.map_files = {
+            "village": "/map/village.txt",
+            "player": "/map/player.txt",
+            "ally": "/map/ally.txt",
+            "conquer": "/map/conquer.txt"
+        }
+
+    def get_files(self, save_location: str = "data/"):
+        for key, value in self.map_files.items():
+            url_get = self.url + value
+            response = requests.get(url_get)
+
+            save_location = save_location + f"en{self.world}/"
+
+            if not os.path.exists(save_location):
+                os.makedirs(save_location)
+            
+            with open(save_location + f"{key}.txt", "w") as f:
+                f.write(response.text)
+            
+            print(f"Saved {key}.txt to {save_location}")
+
+
+        
+
+if __name__ == "__main__":
+
+    tw_api = TWAPI(144, "en")
+
+    tw_api.get_files()
+    
