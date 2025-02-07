89a700e57aa80f942a6fc57e98c7197ad40d66c8
Floris <florisvanrossum@gmail.com>
2025-02-06 23:25:37 -0500

    looking better

--- twmap/map/colors.py
+++ twmap/map/colors.py
@@ -6,81 +6,18 @@ class ColorManager:
     def __init__(self):
         
         self.color_map = {}
-    
-    def generate_unique_colors(self, n: int):
 
-        colors = [
-            "#00FF00",
-            "#0000FF",
-            "#FF0000",
-            "#01FFFE",
-            "#FFA6FE",
-            "#FFDB66",
-            "#006401",
-            "#010067",
-            "#95003A",
-            "#007DB5",
-            "#FF00F6",
-            "#FFEEE8",
-            "#774D00",
-            "#90FB92",
-            "#0076FF",
-            "#D5FF00",
-            "#FF937E",
-            "#6A826C",
-            "#FF029D",
-            "#FE8900",
-            "#7A4782",
-            "#7E2DD2",
-            "#85A900",
-            "#FF0056",
-            "#A42400",
-            "#00AE7E",
-            "#683D3B",
-            "#BDC6FF",
-            "#263400",
-            "#BDD393",
-            "#00B917",
-            "#9E008E",
-            "#001544",
-            "#C28C9F",
-            "#FF74A3",
-            "#01D0FF",
-            "#004754",
-            "#E56FFE",
-            "#788231",
-            "#0E4CA1",
-            "#91D0CB",
-            "#BE9970",
-            "#968AE8",
-            "#BB8800",
-            "#43002C",
-            "#DEFF74",
-            "#00FFC6",
-            "#FFE502",
-            "#620E00",
-            "#008F9C",
-            "#98FF52",
-            "#7544B1",
-            "#B500FF",
-            "#00FF78",
-            "#FF6E41",
-            "#005F39",
-            "#6B6882",
-            "#5FAD4E",
-            "#A75740",
-            "#A5FFD2",
-            "#FFB167",
-            "#009BFF",
-            "#E85EBE"
+        self.colors =  [
+'#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990'
         ]
-        random.shuffle(colors)
-        return colors[:n]
+    def get_unique_color(self):
 
+        return self.colors.pop(0)
+    
     def get_color(self, key: str):
         if key in self.color_map:
             return self.color_map[key]
         else:
-            color = self.generate_unique_colors(1)[0]
+            color = self.get_unique_color()
             self.color_map[key] = color
             return color
