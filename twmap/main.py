6281477243f8ac3864e2d2ff383d8cc747437c18
flipthedog <flipthedog@users.noreply.github.com>
2025-09-27 14:35:07 -0400

    created uv package manager

--- twmap/main.py
+++ twmap/main.py
@@ -3,11 +3,9 @@ import logging
 from twmap.map.mapfactory import MapFactory
 
 # This generates all missing maps for all worlds, using default colors and parameters
-factory = MapFactory("s3://tribalwars-scraped/", max_coords=730)
-factory.generate_missing_maps("en142", regenerate_all=False, max_coords=730)
-factory.generate_missing_maps("en143", regenerate_all=False, max_coords=700)
-factory.generate_missing_maps("en144", regenerate_all=False, max_coords=680)
+factory = MapFactory("s3://tribalwars-scraped/", max_coords=750)
+factory.generate_missing_maps("en142", regenerate_all=False, max_coords=750)
+factory.generate_missing_maps("en143", regenerate_all=False, max_coords=750)
+factory.generate_missing_maps("en144", regenerate_all=False, max_coords=750)
 factory.generate_missing_maps("en145", regenerate_all=False, max_coords=750)
-factory.generate_missing_maps("en146", regenerate_all=False, max_coords=700) 
-factory.generate_missing_maps("enc1", regenerate_all=False, max_coords=650)
-factory.generate_missing_maps("enc2", regenerate_all=False, max_coords=650)
+factory.generate_missing_maps("en146", regenerate_all=False, max_coords=750)
